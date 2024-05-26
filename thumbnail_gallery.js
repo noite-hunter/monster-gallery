6631c62ea6826ae12343d85b1770637705b5db6c
noite-hunter <140098495+noite-hunter@users.noreply.github.com>
2024-05-26 10:05:44 -0300

    All better now
    Changed the html to be generated from a for loop in javascript that pulls from a json database

--- thumbnail_gallery.js
+++ thumbnail_gallery.js
@@ -1,21 +1,23 @@
-import monsterNames from "monster_names.json";
+const response = await fetch("monster_names.json");
+const monsterNames = await response.json();
 
 function species(x){
-   return "0".repeat(3 - x.length) + (x + 1);
-};
+    return (x + 1).toString().padStart(3, "0");
+}
 
+const div = document.getElementById("gallery");
 let generatedGallery = "";
 
-for(let row = 0; row < 10; row++){
-    for(let col = 0; col < 10; col++){
-        if(monsterNames[row][col] = null){ continue };
+for(let row = 0; row < monsterNames.length; row++){
+    for(let col = 0; col <= 9; col++){
+        if(monsterNames[row][col] === null){ continue };
         generatedGallery +=
             `<div class="thumb">
-                <img src="thumbs/thumb-` + species(row) + `_0` + col + `.png" alt="` + monsterNames[row][col] + `" width="400" height="400" />
-                <p><span>` + monsterNames[row][col] + `</span></p>
+                <img src="thumbs/thumb-${species(row)}_0${col}.png" alt="${monsterNames[row][col]}" width="400" height="400" />
+                <p><span>${monsterNames[row][col]}</span></p>
             </div>`
         ;
     };
 };
 
-document.getElementById("gallery").innerHTML = generatedGallery;
+div.insertAdjacentHTML("afterbegin", generatedGallery);
